45a8745eda4935ff2bd7cd8ef18e0061a73000af
Itzanh <itzanh@hotmail.es>
2022-05-17 21:52:17 +0200

    Improve settings validation in front-end

--- orm_models.go
+++ orm_models.go
@@ -6,14 +6,6 @@ func addORMModels() bool {
 	// GORM does not do this automatically
 	dbOrm.Exec("UPDATE pg_opclass SET opcdefault = true WHERE opcname='gin_trgm_ops'")
 
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN IF EXISTS product_barcode_label_width")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN IF EXISTS product_barcode_label_height")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN IF EXISTS product_barcode_label_size")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN IF EXISTS product_barcode_label_margin_top")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN IF EXISTS product_barcode_label_margin_bottom")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN IF EXISTS product_barcode_label_margin_left")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN IF EXISTS product_barcode_label_margin_right")
-
 	err := dbOrm.AutoMigrate(&Country{}, &Language{}, &Currency{}, &Warehouse{}, &Settings{}, &SettingsEcommerce{}, &BillingSerie{}, &PaymentMethod{}, &Account{}, &Journal{},
 		&User{}, &Group{}, &UserGroup{}, &PermissionDictionary{}, &PermissionDictionaryGroup{}, &DocumentContainer{}, &Document{}, &ProductFamily{}, &ProductAccount{},
 		&Carrier{}, &State{}, &Color{}, &Packages{}, &Incoterm{}, &Product{}, &ConfigAccountsVat{}, &ManufacturingOrderType{}, &ManufacturingOrderTypeComponents{},
@@ -31,42 +23,6 @@ func addORMModels() bool {
 		log("AutoMigrate", err.Error())
 		return false
 	}
-
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN ecommerce")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN email")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_url")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_api_key")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_language_id")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_export_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_intracommunity_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_interior_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN sendgrid_key")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN email_from")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN name_from")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_status_payment_accepted")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN prestashop_status_shipped")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN woocommerce_url")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN woocommerce_consumer_key")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN woocommerce_consumer_secret")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN woocommerce_export_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN woocommerce_intracommunity_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN woocommerce_interior_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN woocommerce_default_payment_method")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN shopify_url")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN shopify_token")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN shopify_export_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN shopify_intracommunity_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN shopify_interior_serie")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN shopify_default_payment_method")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN shopify_shop_location_id")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN smtp_identity")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN smtp_username")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN smtp_password")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN smtp_hostname")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN smtp_starttls")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN smtp_reply_to")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN email_send_error_ecommerce")
-	dbOrm.Exec("ALTER TABLE public.config DROP COLUMN email_send_error_sendcloud")
 
 	return true
 }
